4e67b7e30a731d6e95e4cf673edc2ea8be1826cc
Alfakynz <141228129+Alfakynz@users.noreply.github.com>
2024-05-25 16:27:28 +0200

    Start Coding in POO

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import { initScene, initCamera, initRenderer } from './sceneSetup';
-import { createCube } from './shapes';
+import { createCube, createPlane, createBorder } from './shapes';
 
 // Création de la scène, de la caméra et du rendu
 const scene = initScene();
@@ -10,58 +10,26 @@ const renderer = initRenderer();
 // Création du premier cube
 const cube1 = createCube(0x0000ff, -1, 0, 0);
 const cube2 = createCube(0xffffff, 0, 0, 0);
-const cube3 = createCude(0xff0000, 1, 0, 0);
-/*
-const geometry1 = new THREE.BoxGeometry(1, 1, 1);
-const material1 = new THREE.MeshBasicMaterial({ color: 0x0000ff });
-const cube1 = new THREE.Mesh(geometry1, material1);
-cube1.position.set(-1, 0, 0)*/
+const cube3 = createCube(0xff0000, 1, 0, 0);
+const cube4 = createCube(0x0000ff, -1, 1, 0);
+const cube5 = createCube(0xffffff, 0, 1, 0);
+const cube6 = createCube(0xff0000, 1, 1, 0);
+
 scene.add(cube1);
 scene.add(cube2);
 scene.add(cube3);
-
-// Création du deuxième cube
-/*const geometry2 = new THREE.BoxGeometry(1, 1, 1);
-const material2 = new THREE.MeshBasicMaterial({ color: 0xffffff });
-const cube2 = new THREE.Mesh(geometry2, material2);
-cube2.position.set(0, 0, 0); // Positionner le deuxième cube à côté du premier
-scene.add(cube2);*/
-
-// Création du troisième cube
-/*
-const geometry3 = new THREE.BoxGeometry(1, 1, 1);
-const material3 = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const cube3 = new THREE.Mesh(geometry3, material3);
-cube3.position.set(1, 0, 0); // Positionner le deuxième cube à côté du premier
-scene.add(cube3);*/
-
-// Création du quatrième cube
-const geometry4 = new THREE.BoxGeometry(1, 1, 1);
-const material4 = new THREE.MeshBasicMaterial({ color: 0x0000ff });
-const cube4 = new THREE.Mesh(geometry4, material4);
-cube4.position.set(-1, 1, 0)
 scene.add(cube4);
-
-// Création du cinquième cube
-const geometry5 = new THREE.BoxGeometry(1, 1, 1);
-const material5 = new THREE.MeshBasicMaterial({ color: 0xffffff });
-const cube5 = new THREE.Mesh(geometry5, material5);
-cube5.position.set(0, 1, 0); // Positionner le deuxième cube à côté du premier
 scene.add(cube5);
-
-// Création du sixième cube
-const geometry6 = new THREE.BoxGeometry(1, 1, 1);
-const material6 = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const cube6 = new THREE.Mesh(geometry6, material6);
-cube6.position.set(1, 1, 0); // Positionner le deuxième cube à côté du premier
 scene.add(cube6);
 
 // Création du sol
+// const plane = createPlane(0x808080, 0, -1, 0);
 const planeGeometry = new THREE.BoxGeometry(10, 10, 1);
 const planeMaterial = new THREE.MeshBasicMaterial({ color: 0x808080 }); // Couleur grise
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 plane.rotation.x = -Math.PI / 2; // Rotation pour placer le plan horizontalement
 plane.position.y = -1; // Positionner le plan sous les cubes
+
 scene.add(plane);
 
 // Variables pour suivre la position de la caméra
@@ -77,129 +45,70 @@ let goDown = false;
 const cameraDirection = new THREE.Vector3();
 
 // Fonction pour gérer les commandes de déplacement et de rotation
+const keysPressed = {};
+
+// Gestion des touches pressées et relâchées
+document.addEventListener('keydown', (event) => {
+	keysPressed[event.key] = true;
+});
+
+document.addEventListener('keyup', (event) => {
+	keysPressed[event.key] = false;
+});
+
 function handleControls() {
 	// Obtenir la direction de la caméra
 	camera.getWorldDirection(cameraDirection);
 
 	// Avancer
-	if (moveForward) {
-		camera.position.add(cameraDirection.multiplyScalar(moveSpeed));
+	if (keysPressed['z']) {
+		const forwardDirection = new THREE.Vector3(cameraDirection.x, 0, cameraDirection.z).normalize();
+		camera.position.add(forwardDirection.clone().multiplyScalar(moveSpeed));
 	}
 	// Reculer
-	if (moveBackward) {
-		camera.position.sub(cameraDirection.clone().multiplyScalar(moveSpeed));
+	if (keysPressed['s']) {
+		const forwardDirection = new THREE.Vector3(-cameraDirection.x, 0, -cameraDirection.z).normalize();
+		camera.position.add(forwardDirection.clone().multiplyScalar(moveSpeed));
 	}
 	// Tourner à gauche
-	if (turnLeft) {
-		const leftDirection = new THREE.Vector3(-cameraDirection.z, 0, cameraDirection.x).normalize();
-		camera.position.sub(leftDirection.multiplyScalar(moveSpeed));
+	if (keysPressed['q']) {
+		const leftDirection = new THREE.Vector3(cameraDirection.z, 0, -cameraDirection.x).normalize();
+		camera.position.add(leftDirection.clone().multiplyScalar(moveSpeed));
 	}
 	// Tourner à droite
-	if (turnRight) {
-		const rightDirection = new THREE.Vector3(cameraDirection.z, 0, -cameraDirection.x).normalize();
-		camera.position.sub(rightDirection.multiplyScalar(moveSpeed));
+	if (keysPressed['d']) {
+		const rightDirection = new THREE.Vector3(-cameraDirection.z, 0, cameraDirection.x).normalize();
+		camera.position.add(rightDirection.clone().multiplyScalar(moveSpeed));
 	}
 	// Monter
-	if (goUp) {
+	if (keysPressed[' ']) {
 		const upDirection = new THREE.Vector3(0, 1, 0);
 		camera.position.add(upDirection.clone().multiplyScalar(moveSpeed));
 	}
-	if (goDown) {
-		const upDirection = new THREE.Vector3(0, 1, 0);
-		camera.position.sub(upDirection.clone().multiplyScalar(moveSpeed));
+	// Descendre
+	if (keysPressed['Shift']) {
+		const upDirection = new THREE.Vector3(0, -1, 0);
+		camera.position.add(upDirection.clone().multiplyScalar(moveSpeed));
 	}
 }
-
-// Gestion des touches pressées et relâchées
-document.addEventListener('keydown', (event) => {
-	switch (event.key) {
-		case 'z':
-			moveForward = true;
-			break;
-		case 's':
-			moveBackward = true;
-			break;
-		case 'q':
-			turnLeft = true;
-			break;
-		case 'd':
-			turnRight = true;
-			break;
-		case ' ':
-			goUp = true;
-			break;
-		case 'Shift':
-			goDown = true;
-			break;
-	}
-});
-
-document.addEventListener('keyup', (event) => {
-	switch (event.key) {
-		case 'z':
-			moveForward = false;
-			break;
-		case 's':
-			moveBackward = false;
-			break;
-		case 'q':
-			turnLeft = false;
-			break;
-		case 'd':
-			turnRight = false;
-			break;
-		case ' ':
-			goUp = false;
-			break;
-		case 'Shift':
-			goDown = false;
-			break;
-	}
-});
 
 camera.position.z = 2;
 
 // Ajout des bordures aux cubes
-const border1 = new THREE.LineSegments(
-	new THREE.EdgesGeometry(cube1.geometry),
-	new THREE.LineBasicMaterial({ color: 'white' })
-);
+const border1 = createBorder(cube1, 'white');
+const border2 = createBorder(cube2, 'white');
+const border3 = createBorder(cube3, 'white');
+const border4 = createBorder(cube4, 'white');
+const border5 = createBorder(cube5, 'white');
+const border6 = createBorder(cube6, 'white');
+const border = createBorder(plane, 'white');
+
 cube1.add(border1);
-
-const border2 = new THREE.LineSegments(
-	new THREE.EdgesGeometry(cube2.geometry),
-	new THREE.LineBasicMaterial({ color: 'white' })
-);
 cube2.add(border2);
-
-const border3 = new THREE.LineSegments(
-	new THREE.EdgesGeometry(cube3.geometry),
-	new THREE.LineBasicMaterial({ color: 'white' })
-);
 cube3.add(border3);
-
-const border4 = new THREE.LineSegments(
-	new THREE.EdgesGeometry(cube4.geometry),
-	new THREE.LineBasicMaterial({ color: 'white' })
-);
 cube4.add(border4);
-
-const border5 = new THREE.LineSegments(
-	new THREE.EdgesGeometry(cube5.geometry),
-	new THREE.LineBasicMaterial({ color: 'white' })
-);
 cube5.add(border5);
-
-const border6 = new THREE.LineSegments(
-	new THREE.EdgesGeometry(cube6.geometry),
-	new THREE.LineBasicMaterial({ color: 'white' })
-);
 cube6.add(border6);
-
-const border = new THREE.LineSegments(
-	new THREE.EdgesGeometry(plane.geometry),
-	new THREE.LineBasicMaterial({ color: 'white' })
-);
 plane.add(border);
 
 // Variables pour suivre la rotation de la caméra
--- shapes.js
+++ shapes.js
@@ -1,9 +1,48 @@
 import * as THREE from 'three';
 
+class Shape {
+    constructor() {
+        this.geometry = null;
+        this.material = null;
+        this.mesh = null;
+    }
+}
+
+class Cube extends Shape {
+    constructor(color, x, y, z) {
+        super();
+        this.geometry = new THREE.BoxGeometry(1, 1, 1);
+        this.material = new THREE.MeshBasicMaterial({ color: color });
+        this.mesh = new THREE.Mesh(this.geometry, this.material);
+        this.mesh.position.set(x, y, z);
+    }
+}
+
+class Plane extends Shape {
+    constructor(color, x, y, z) {
+        super();
+        this.geometry = new THREE.BoxGeometry(10, 1, 10);
+        this.material = new THREE.MeshBasicMaterial({ color: color });
+        this.mesh = new THREE.Mesh(this.geometry, this.material);
+        this.mesh.rotation.x = -Math.PI / 2;
+        this.mesh.position.set(x, y, z);
+    }
+}
+
 export function createCube(color, x, y, z) {
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: color });
-    const cube = new THREE.Mesh(geometry, material);
-    cube.position.set(x, y, z)
-    return cube;
+    const cube = new Cube(color, x, y, z);
+    return cube.mesh;
+}
+
+export function createPlane(color, x, y, z) {
+    const plane = new Plane(color, x, y, z);
+    return plane.mesh;
+}
+
+export function createBorder(shape, color) {
+    const border = new THREE.LineSegments(
+        new THREE.EdgesGeometry(shape.geometry),
+        new THREE.LineBasicMaterial({ color: color })
+    );
+    return border;
 }
